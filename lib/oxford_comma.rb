7aa2ccc8fc24530ab42a8c322a6e4ac4fc013a44
mborreros <mlbsindia@gmail.com>
2022-02-25 20:24:34 -0500

    commit tree 02c99702e0e884772becf3f526443b71b6867d2e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  else array.count > 2
+    oxford_comma = []
+    og_final_element = array[-1]
+    shortened_array = array.pop
+    shortened_with_commas = array.join(", ")
+    transformed_final_element = og_final_element.insert(0, ", and ")
+    oxford_comma << shortened_with_commas
+    oxford_comma << transformed_final_element
+    result = oxford_comma.join
+    result
+  end
 end
